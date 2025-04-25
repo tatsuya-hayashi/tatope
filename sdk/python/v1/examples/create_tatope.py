a4046221bf2dfe7bcbbe97de5cd4c78a25f22048
tatsuya-hayashi <tatsuya.hayashi@hpe.com>
2025-04-24 19:06:57 -0700

    wip

--- sdk/python/v1/examples/create_tatope.py
+++ sdk/python/v1/examples/create_tatope.py
@@ -3,17 +3,18 @@ from kubernetes.client import V1ObjectMeta
 
 from tatope.models import TatOpe, TatOpeSpec, TatOpeStatus
 
+config.load_kube_config()
+
 t = TatOpe(
-    kind="TatOpe"
-    api_version="tatope.tatope.local/v1"
+    kind="TatOpe",
+    api_version="tatope.tatope.local/v1",
     metadata=V1ObjectMeta(
-        name="hohoho"
-        namespace="default"
+        name="hohoho",
+        namespace="default",
     ),
     spec=TatOpeSpec(
         foo="fooval1",
         hoge="hogeval1",
-        ports=[12,34]
-
+        ports=[12,34],
     )
 )
